8e6c7d20b87542a43e04ceba9a5b88de21574754
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-09-07 15:57:44 +0330

    feat: responsive video

--- components/body/Profile.jsx
+++ components/body/Profile.jsx
@@ -114,15 +114,17 @@ const Profile = () =>
             </div>
             <div className="border border-[#34ABE3] "></div>
             <Title>تازه‌ها</Title>
-            <div className="flex">
-              <div className="flex items-center border-b-2 border-black pb-3">
+            <div className="md:flex mr-5 text-[13px] md:text-[.7rem] ">
+              <div className="flex items-center md:border-b-2 w-[65px] border-black pb-2 md:pr-2 ">
+                <div className="border-2 h-9 border-[#34ABE3] ml-3 md:hidden"></div>
                 <FaRegCopy className="text-[16px] ml-2" /> همه
               </div>
-              <div className="flex items-center border-b-2 text-gray-400 pb-3 ">
-                <CiStar className="text-xl ml-2 mr-4" /> برجسته‌ها
+              <div className="flex items-center md:border-b-2 md:w-[110px] text-gray-400 pb-2 bg-gray-100 md:bg-opacity-0 ">
+                <div className="border-2 h-9 border-gray-300 ml-3 md:hidden"></div>
+                <CiStar className="text-xl ml-2 md:mr-3" /> برجسته‌ها
               </div>
             </div>
-            <div className="border rounded-[6px] pt-8 pb-2 mt-8 ">
+            <div className="border border-gray-400 rounded-[6px] pt-[22px] pb-2 mt-8 ">
               <div className="flex items-center justify-between px-4 mb-8">
                 <div className="flex items-center">
                   <Image
@@ -136,9 +138,13 @@ const Profile = () =>
                 </div>
                 <AiOutlineEllipsis className="text-xl text-gray-400" />
               </div>
-              <video controls src={"/dev.mp4"} className="w-56 mx-auto h-52" />
+              <video
+                controls
+                src={"/dev.mp4"}
+                className="w-[80%] mx-auto h-48"
+              />
               <div className="flex justify-center pt-2 ">
-                <div className="flex items-center justify-between w-56">
+                <div className="flex items-center justify-between w-[80%]">
                   <div className="flex items-center text-gray-500">
                     <FaRegComment className="text-xl ml-2 " /> 0
                     <FiHeart className="text-xl ml-2 mr-4 " /> 5
--- components/body/Title.jsx
+++ components/body/Title.jsx
@@ -3,7 +3,7 @@ import { FaAngleUp } from "react-icons/fa";
 const Title = ({ children }) => {
   return (
     <div className="flex justify-between px-2 py-4">
-      <p className="text-[#34ABE3] text-[.7rem]">{children}</p>
+      <p className="text-[#34ABE3] text-[.7rem] font-bold">{children}</p>
       <FaAngleUp className="text-gray-400" />
     </div>
   );
